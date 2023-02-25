79169925d6595142272fc52d2aa9acd440760ab7
AlanLee <1445654576@qq.com>
2023-02-25 16:00:19 +0800

    feat: add 验证二叉搜索树

--- LeetBook/初级算法/04-树/02-验证二叉搜索树.js
+++ LeetBook/初级算法/04-树/02-验证二叉搜索树.js
@@ -14,31 +14,46 @@
  */
 
 const isValidBST = function (root) {
-  return handleValidBST(root, null)
+  return handleValidBST(root)
 };
 
-function handleValidBST(root, preRoot) {
-  if(root === null) return true
-  let flag = true
-  if(root.left) {
-    let left = root.left
-    if(root.val <= left.val) return false
-    if(root.val > left.val && preRoot && preRoot.val >= left.val) flag = false
-    if(left.left) flag = handleValidBST(left, root)
-    if(left.right) flag = handleValidBST(left, root)
-  }
-  if(root.right) {
-    let right = root.right
-    if(root.val >= right.val) return false
-    if(root.val < right.val && preRoot && preRoot.val < right.val) {
-      return false
-    } else {
-      flag = true
-    }
-    if(right.right) flag = handleValidBST(right, root)
-  }
-  return flag
+function handleValidBST(root, min = -Infinity, max = Infinity) {
+  if(!root) return true;
+  if(root.val <= min || root.val >= max) return false;
+  return handleValidBST(root.left, min, root.val) && handleValidBST(root.right, root.val, max);
 }
+
+// function handleValidBST(root, preRoot, isLeft = false) {
+//   if(root === null) return true
+//   let flag = true
+//   if(root.left) {
+//     let left = root.left
+//     if(root.val <= left.val) return false
+//     if(root.val > left.val && preRoot && preRoot.val >= left.val && !isLeft) return false
+//     if(left.left) {
+//       flag = handleValidBST(left, root, true)
+//       if(!flag) return false
+//     }
+//     if(left.right) {
+//       flag = handleValidBST(left, root, true)
+//       if(!flag) return false
+//     }
+//   }
+//   if(root.right) {
+//     let right = root.right
+//     if(root.val >= right.val) return false
+//     if(root.val < right.val && preRoot && preRoot.val < right.val && isLeft) {
+//       return false
+//     } else {
+//       flag = true
+//     }
+//     if(right.right) {
+//       flag = handleValidBST(right, root)
+//       if(!flag) return false
+//     }
+//   }
+//   return flag
+// }
 
 
 const root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
@@ -49,8 +64,10 @@ const root5 = new TreeNode(32, new TreeNode(26, new TreeNode(19, null, new TreeN
 const root6 = new TreeNode(3, new TreeNode(1, new TreeNode(0), new TreeNode(2)), new TreeNode(5, new TreeNode(4), new TreeNode(6)));
 
 // console.log('root', root);
-console.log('root6', root6);
 // console.log(isValidBST(root));
 // console.log(isValidBST(root2));
 // console.log(isValidBST(root3));
+// console.log('root5', root5);
+// console.log('root6', root6);
+console.log(isValidBST(root5));
 console.log(isValidBST(root6));
